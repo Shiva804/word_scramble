b710d78aae36d4050bdaf37e081d2e272f0e7fb1
Shiva804 <krishnansai49@gmail.com>
2020-08-10 15:26:53 +0530

    Updates

---
+++ precache-manifest.5371364a77ccfe4a37f496469991c897.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3014679a083a6156f45242ca9c8d8c7b",
+    "url": "/word_scramble/index.html"
+  },
+  {
+    "revision": "ce3a1e30458f5f261a32",
+    "url": "/word_scramble/static/css/main.cbf4eb01.chunk.css"
+  },
+  {
+    "revision": "3f8dad641e0e9da1b791",
+    "url": "/word_scramble/static/js/2.5b07261f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/word_scramble/static/js/2.5b07261f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3a1e30458f5f261a32",
+    "url": "/word_scramble/static/js/main.9a3c56ed.chunk.js"
+  },
+  {
+    "revision": "de1ee72e419ed06f3a05",
+    "url": "/word_scramble/static/js/runtime-main.4af7e0bc.js"
+  },
+  {
+    "revision": "6152576c234d080ac244df533efb374c",
+    "url": "/word_scramble/static/media/pexels-ann-h-3482443.6152576c.jpg"
+  },
+  {
+    "revision": "5d50adac3fb5e6ec88fb5ba88aea091f",
+    "url": "/word_scramble/static/media/pexels-anni-roenkae-2457284.5d50adac.jpg"
+  },
+  {
+    "revision": "a76947d898f77a2418ae18f417c5a451",
+    "url": "/word_scramble/static/media/pexels-dids-1424495.a76947d8.jpg"
+  },
+  {
+    "revision": "2d30a497ba35df1358da8bfca90eb265",
+    "url": "/word_scramble/static/media/select1.2d30a497.png"
+  }
+]);
